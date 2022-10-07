a18c39b0db122b98a0338d937e728ba76754cc78
rajeshmanjunath <rajesh@thisisalice.com>
2022-10-07 13:07:57 +0530

    inserting rows

--- bookstore_users_api/domain/users/user_dao.go
+++ bookstore_users_api/domain/users/user_dao.go
@@ -2,15 +2,21 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/raj23manj/fed-golang-microservices/bookstore_users_api/datasources/mysql/users_db"
 	// utils "github.com/raj23manj/fed-golang-microservices/bookstore_users_api/utils/date"
+
 	"github.com/raj23manj/fed-golang-microservices/bookstore_users_api/utils/date"
 	"github.com/raj23manj/fed-golang-microservices/bookstore_users_api/utils/errors"
 )
 
 var (
 	usersDB = make(map[int64]*User)
+)
+
+const (
+	queryInsertUser = ("INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);")
 )
 
 // not passing a  pointer, but passing a copy of the value from the callee
@@ -36,15 +42,45 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	// when to use prepare & exec() directly, section 3, how to insert rows 14:16
+	// if need to resuse prepare again then use prepare stmt
+	// prepare is used to validate the query before inhand
+	// prepare and exec has better performance
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
-	usersDB[user.Id] = user
+	defer stmt.Close()
 	user.DateCreated = date.GetNowString()
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		// Error 1062 (23000): Duplicate entry 'example@demo.com' for key 'users.email_unique'"
+		if strings.Contains(err.Error(), "email_unique") {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	// instead of uing above statements use below
+	// result, err := users_db.Client.Exec(queryUpdateUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
+
+	user.Id = userId
+
+	// current := usersDB[user.Id]
+	// if current != nil {
+	// 	if current.Email == user.Email {
+	// 		return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	// 	}
+	// 	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	// }
+
+	// usersDB[user.Id] = user
+	// user.DateCreated = date.GetNowString()
 	return nil
 }
